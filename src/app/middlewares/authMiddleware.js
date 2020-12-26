083d119acd23269a69d2af622303d5f32d117880
Renan Lisboa <renlp89.developer@gmail.com>
2020-12-26 12:43:06 -0300

    added new feature: authentication

--- src/app/middlewares/authMiddleware.js
+++ src/app/middlewares/authMiddleware.js
@@ -1,14 +1,23 @@
-// import jwt from 'jsonwebtoken';
-// import { promisify } from 'util';
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
 
-// import authConfig from '../../config/auth';
+import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
-  // VALIDATIONS GOES HERE
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided.' });
+  }
+
+  const [, token] = authHeader.split(' ');
 
   try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+
     return next();
   } catch (err) {
-    return res.json();
+    return res.status(400).json({ error: 'Invalid token.' });
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/authMiddleware';
+
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
 routes.get('/users/:id', UserController.show);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
