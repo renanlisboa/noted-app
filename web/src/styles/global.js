bc8a1daca58534a8d0d9501c6c6d8a9f8d1b2dc6
Renan Lisboa <renlp89.developer@gmail.com>
2021-02-04 06:34:49 -0300

    added project global styles

--- web/src/styles/global.js
+++ web/src/styles/global.js
@@ -4,31 +4,43 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    outline: 0;
     box-sizing: border-box;
   }
 
-  html, body, #root {
-    min-height: 100%;
-  }
-
   body {
+    min-height: 100%;
     background: #fff;
     -webkit-font-smoothing: antialiased !important;
   }
 
-  /* h1, h2, h3, h4, h5, h6, p, span, a {
-    color: #222;
-  } */
+  *, button, input {
+    border: 0;
+    outline: 0;
+    font-family: 'Lato', sans-serif;
+  }
 
-  /* body, input {
-    color: #222;
-    font-size: 16px;
-    font-family: Arial, Helvetica, sans-serif;
-  } */
+  :root {
+    --primary: #f5f5f5;
+    --secondary: #ffffff;
+    --terceary: #1e90ff;
 
-  /* button {
-    color: #222;
+    --text: #000000;
+    --light-text: #333333;
+    --input-field: #eeeeee;
+    --input-text: #999999;
+
+    --success: #04d361;
+    --error: #ea2121;
+    --warning: #f8921e;
+  }
+
+  h1, h2, h3, p, svg {
+    color: --var(--text);
+  }
+
+  button {
+    background: var(--terceary);
+    color: var(--secondary);
     cursor: pointer;
-  } */
+  }
 `;
