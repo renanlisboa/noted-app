cfce379d713842e902ca3964f79891c69e7870cf
Renan Lisboa <renlp89.developer@gmail.com>
2021-02-10 16:44:02 -0300

    added cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 import './database';
@@ -9,6 +10,7 @@ const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use(cors());
 app.use(
   '/files',
   express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
